6d69a3e2aaa709c765c64420073ac448a4a2a76a
Ar Rakin <rakinar2@gmail.com>
2022-04-10 15:52:40 +0600

    Add about page

---
+++ about/index.php
@@ -0,0 +1,74 @@
+<?php
+    $TITLE = "About &hyphen; The Everything Server";
+    $PREFIX = '..';
+    require("../header.php");
+    require("../navbar.php");
+?>
+
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <h1>About The Everything Server</h1>
+            <p>This is a community server for everything.</p>
+            <p>
+                This server is a place for pretty much everything. The unofficial official place for Facebook, Soundcloud and Snapchat and channels for a lot of different things including: 
+            </p>
+            <ul>
+                <li>Create arts</li>
+                <li>Sports</li>
+                <li>Hobbies, and gaming</li>
+                <li>Social media and apps</li>
+                <li>Fun stuff</li>
+                <li>Education</li>
+                <li>News</li>
+                <li>open for partnerships</li>
+                <li>+Much more!</li>
+            </ul>
+            <a href="https://discord.gg/dKArUwBMKw" class="btn btn-primary">Join Us Now!</a>
+            <br>
+            <br>
+            <br>
+
+            <h3>Boosting the server</h3>
+            <p>We'll be very greatful if you boost this server.Boosters will get some advantages:</p>
+            <ul>
+                <li>All boosters will be hoisted between staff and general users.</li>
+                <li>You will get your own special role here and the choice of colour</li>
+                <li>You will get the choice of stickers once we gain access to them plus special emojis /ability to choose emojis </li>
+                <li>You will have have more say in what events we have and other server contributions</li>
+                <li>You will have a higher chance of getting sotd selection</li> 
+                <li>You will have a chat channel just for boosters </li>
+                <li>You will be helping this server to be better not only for us but for everyone in it</li>
+                <li>More perms to come</li>
+            </ul>
+
+            <br>
+            <br>
+            
+            <h3>Rules</h3>
+            <p>Let's talk about the rules now. You must follow this rules in order to continue with our server. If you don't, then we might verbally warn you, and if you still continue to break these rules, then we might mute/kick/ban you.</p>
+            <ul>
+                <li>Don't be toxic in the chat</li>
+                
+                <li>Don't excessively spam or degrade the quality of the chat. Go to #spam-chat you want to spam but read pins</li>
+                
+                <li>Though this is a server for everything it is asked that you refrain from discussing illegal or extremely sensitive stuff. Political chat is allowed here but only in the specified channel.</li>
+                
+                <li>Please make your name pingable, non nsfw and appropriate. Multiple warnings regarding your username will result in a removal of name changing perms here.</li>
+                
+                <li>If you have been banned here and you rejoin with an alt your alt will also be banned.</li>
+                
+                <li>This server is a SFW server so please refrain from NSFW conversations or NSFW content.</li>
+                
+                <li>Please do not post images of other users without their permission here.</li>
+            </ul>
+
+            <br>
+            <br>
+        </div>
+    </div>
+</div>
+
+<?php
+    require("../footer.php");
+?>
