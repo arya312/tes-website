4dcb371ff06bdb54bb9b63d82191d9d0474cf5a8
Ar Rakin <rakinar2@gmail.com>
2022-04-10 15:57:14 +0600

    Updated OG Meta Tags

--- header.php
+++ header.php
@@ -12,6 +12,10 @@
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="<?= $PREFIX ?? '.' ?>/assets/css/style.css">
 
+    <meta name="description" content="A full-featured discord community server for everything discussions!">
+    <meta name="og:title" content="<?= $TITLE ?>">
+    <meta name="og:description" content="A full-featured discord community server for everything discussions!">
+
     <title><?= $TITLE ?></title>
   </head>
   <body>
