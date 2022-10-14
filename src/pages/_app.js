1021090e9fb399608c2eebb2ccc4bb878f88aa1c
Ar Rakin <rakinar2@gmail.com>
2022-10-14 08:19:45 +0000

    feat: add montserrat font

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,18 @@
+import { createTheme, ThemeProvider } from '@mui/material';
 import '../css/globals.css'
 import MainLayout from '../layouts/MainLayout';
 
 function MyApp({ Component, pageProps }) {
-    return Component.layout ? <Component.layout><Component {...pageProps} /></Component.layout> : 
-                Component.layout === false ? <Component {...pageProps} /> : <MainLayout><Component {...pageProps} /></MainLayout>;
+    const theme = createTheme({
+        typography: {
+            fontFamily: "Montserrat"
+        }
+    });
+
+    return <ThemeProvider theme={theme}>
+        {Component.layout ? <Component.layout><Component {...pageProps} /></Component.layout> : 
+                Component.layout === false ? <Component {...pageProps} /> : <MainLayout><Component {...pageProps} /></MainLayout>}
+    </ThemeProvider>;
 }
 
 export default MyApp
