6310e1e28a4c3fb3835d46b0fb5e66fb66805aa1
Ar Rakin <rakinar2@gmail.com>
2022-10-15 14:26:08 +0000

    fix(config)!: mailbot url

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export const THEME_COLOR = '#f14a60';
 export const THEME_COLOR_DARKER = '#A83343';
 export const INVITE_URL = "https://discord.gg/tBf3Bx5Ghm";
-export const MAILBOT_URL = "";
+export const MAILBOT_URL = "https://discord.com/channels/@me/959722997143457842";
