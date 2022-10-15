2f91221d5ef053c0084af406c5cfe685dec2bb6a
Ar Rakin <rakinar2@gmail.com>
2022-10-15 20:46:32 +0600

    feat: add additional meta tags

--- src/layouts/MainLayout.js
+++ src/layouts/MainLayout.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
+import { THEME_COLOR } from '../config';
 import icon from '../images/logo-circle.png';
 
 export default function MainLayout({ children }) {
@@ -10,6 +11,11 @@ export default function MainLayout({ children }) {
                 <link rel="icon" href={icon.src} /> 
                
                 <meta name="description" content="A Community for Everything You Can Imagine - The Everything Server." />
+                <meta name="og:color" content={THEME_COLOR} />
+                <meta name="color" content={THEME_COLOR} />
+                <meta name="theme-color" content={THEME_COLOR} />
+                <meta name="og:image" content={icon.src} />
+                <meta name="image" content={icon.src} />
                 <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
                 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
             </Head>
